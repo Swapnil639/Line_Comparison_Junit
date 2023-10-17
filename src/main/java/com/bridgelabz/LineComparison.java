d30cd8788513c2f05de1b821f74d6ab0b93ce849
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-17 06:48:12 +0530

    Added:Calculate length of line testing

--- src/main/java/com/bridgelabz/LineComparison.java
+++ src/main/java/com/bridgelabz/LineComparison.java
@@ -2,7 +2,9 @@ package com.bridgelabz;
 
 
 public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Junit testing");
+    public double lengthOfLineCalculation(double X1, double X2, double Y1, double Y2) {
+
+        double lengthOfLine = Math.sqrt(Math.pow((X2 - X1), 2) + Math.pow((Y2 - Y1), 2));
+        return lengthOfLine;
     }
 }
--- src/test/java/com/bridgelabz/LineComparisonTest.java
+++ src/test/java/com/bridgelabz/LineComparisonTest.java
@@ -1,5 +1,15 @@
 package com.bridgelabz;
-public class LineComparisonTest {
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LineComparisonTest {
+    LineComparison obj = new LineComparison();
+
+    @Test
+    public void givenInputCalculatedLengthOfLines() {
+        double res = obj.lengthOfLineCalculation(1.2, 2.2, 3.2, 4.2);
+        Assertions.assertEquals(1.4142135623730951, res);
+    }
 
 }
